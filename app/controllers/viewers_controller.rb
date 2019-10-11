41c32ccd37d04503d49f7462fa7dba584f46ba22
mitzvahgirl <mitzvahgirlprod@gmail.com>
2019-10-11 13:04:32 -0400

    added destroy methid to viewers_controller.rb

--- app/controllers/viewers_controller.rb
+++ app/controllers/viewers_controller.rb
@@ -13,28 +13,12 @@ end
  end
 end    
 
-
-#    def create
-#     viewer = Viewer.new(
-#         name: params[:name],
-#         email: params[:email],
-#         password: params[:password],
-#         password_confirmation: params[:password_confirmation])
-
-#         if viewer.save
-#             session[:viewer_id] = viewer_id
-#             flash[:success] = "Successfully Created Viewer Account"
-#         else
-#             flash[:warning] = "Invalid email or Password"
-#             redirect_to  '/signup'
-#         end
-#     end
     
-#     def destroy
-#         session[:user_id] = nil
-#         flash[:success] = “Successfully Logged Out!”
-#         redirect_to ‘/login’
+     def destroy
+         session[:user_id] = nil
+         flash[:success] = “Successfully Logged Out!”
+       redirect_to ‘/login’
 
-#       end
+      end
 
     end
