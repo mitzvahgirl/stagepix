d1a93027674adddcd4778f72595916bc541e305f
mitzvahgirl <mitzvahgirlprod@gmail.com>
2019-10-10 22:08:01 -0400

    added new.erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,67 +4,66 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    
-# index   
-
-get '/ posts' do
-  @posts = posts.all
-  erb : 'posts/index'
-end
-
-# new
-get 'posts/new' do
- erb :'posts/new'
-end  
-
-# create 
-   post '/posts' do
-    viewer = viewer.find_by_id([params[:viewer_id])
-    if @post.save
-      redirect '/posts'
-    else
-      redirect '/posts/new'
- end
-
-
-# show
-get "posts/:id" do
-  @post = Post.find_by_id(params[:id])
-  if @post
-    erb :'posts/show'
-  else
-    redirect 'posts'
+    set :views, "app/views"
   end
-end
+
+# # index   
+# get '/ viewers' do
+#   @viewers = viewers.all
+#   erb :'viewers/index'
+# end
+
+# # new
+# get 'viewers/new' do
+#  erb :'viewers/new'
+# end  
+
+# # create 
+#    post '/viewers' do
+#     viewer = Viewer.find_by_id(params[:viewer_id])
+#     if @post.save
+#       redirect '/viewers'
+#     else
+#       redirect '/viewers/new'
+#  end
+# end
 
 
-# edit
-get '/posts/:id' do
-   @post = Post.find_by_id([params[:id]
-   erb: "posts/edit"
-end
-
-# update
-get 'post/update'
- 
-end
-patch "posts/:id" do
-  @post = Post.find_by_id([params[:id]
-  if @post.update(title: params[:title], body: params[:body])
-redirect 'postss/#{@post.id}'
-  else
-redirect 'posts/new'
-  end
-end
+# # show
+# get "viewers/:id" do
+#   @post = Post.find_by_id(params[:id])
+#   if @post
+#     erb :'viewers/show'
+#   else
+#     redirect 'viewers'
+#   end
+# end
 
 
-# delete
-@delete "/posts/:id" do
-  Post.destroy(params[:id])
-  redirect to "/posts"
-end
+# # edit
+# get '/viewers/:id' do
+#    @post = Post.find_by_id(params[:id])
+#    erb :"viewers/edit"
+# end
 
+# # update
+# get 'viewers/update' do
+# end
 
-end
+# # patch "viewers/:id" do
+# #   @post = Post.find_by_id([params[:id]
+# #   @post.update(title: params[:title], body: params[:body])
+# #   if @post.errors.empty?
+# # redirect 'posts/#{@post.id}'
+# #   else
+# # redirect 'posts/new'
+# #   end
+# # end
+
+# # delete
+#   delete "/viewers/:id" do
+#   Post.destroy(params[:id])
+#   redirect "/viewers"
+# end
 
 end
--- app/controllers/viewers_controller.rb
+++ app/controllers/viewers_controller.rb
@@ -1,28 +1,40 @@
 class ViewersController < ApplicationController
-    def new
-    end
-    
-   def create
-    viewer = Viewer.new(
-        name: params[:name],
-        email: params[:email],
-        password: params[:password],
-        password_confirmation: params[:password_confirmation]
+get '/viewers/new' do 
+ erb :'viewers/new'
+end
 
-        if viewer.save
-            session[:viewer_id] = viewer_id
-            flash[:success] = "Successfully Created Viewer Account"
-        else
-            flash[:warning] = "Invalid email or Password"
-            redirect_to  '/signup'
-        end
-    end
+ # create 
+   post '/viewers' do
+    @viewer = Viewer.new(params[:viewer_id], [:name], [:email], [:password])
+    if @viewer.save
+      redirect '/viewers'
+    else
+      redirect '/viewers/new'
+ end
+end    
+
+
+#    def create
+#     viewer = Viewer.new(
+#         name: params[:name],
+#         email: params[:email],
+#         password: params[:password],
+#         password_confirmation: params[:password_confirmation])
+
+#         if viewer.save
+#             session[:viewer_id] = viewer_id
+#             flash[:success] = "Successfully Created Viewer Account"
+#         else
+#             flash[:warning] = "Invalid email or Password"
+#             redirect_to  '/signup'
+#         end
+#     end
     
-    def destroy
-        session[:user_id] = nil
-        flash[:success] = “Successfully Logged Out!”
-        redirect_to ‘/login’
-       end
-      end
+#     def destroy
+#         session[:user_id] = nil
+#         flash[:success] = “Successfully Logged Out!”
+#         redirect_to ‘/login’
+
+#       end
 
     end
